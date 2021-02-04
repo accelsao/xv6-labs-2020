3cc476f7a0e0b565a4edd71353bf1d23e7b327ec
accelsao <jayzhan211@gmail.com>
2021-02-04 09:40:00 +0800

    bttest: done

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,15 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace(void) {
+  uint64 fp = r_fp();
+  printf("backtrace: \n");
+  uint64 low = PGROUNDDOWN(fp);
+  uint64 high = PGROUNDUP(fp);
+  while(low <= fp && fp < high) {
+    printf("%p\n", *((uint64 *)(fp - 8)));
+    fp = *((uint64 *)(fp - 16));
+
+  }
+}
